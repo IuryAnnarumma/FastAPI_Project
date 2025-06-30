dccd421a49bc3c303652d1f8762af9a48662fee0
iury.annarumma <iury.annarumma@tisafe.com>
2025-06-30 17:31:03 -0300

    [new] main created and app configurations

--- main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI() # Criação da Instância
-
-@app.get("/") # Decorador de rota, que será responsável por tratar as requisições que vão para a rota "/" usando o operador get
-
